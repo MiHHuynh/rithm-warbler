0d980179613b24d3ca18f0c7217605c4234c17bf
Michelle Huynh <michhh.12@gmail.com>
2017-10-24 16:53:12 -0700

    started delete

--- warbler-app/models/warble.js
+++ warbler-app/models/warble.js
@@ -29,22 +29,12 @@ warbleSchema.pre('save', function(next) {
 
 warbleSchema.pre('remove', function(next) {
   let warble = this;
-  console.log(warble);
-  if (warble.isNew) { // ???
-    mongoose.model('User').findById(warble.user_id)
-    .then(function(user) {
-      user.warbles.(warble._id); // concat the separate parts of it??
-      return user.save();
-    })
-    .then(function() {
-      next();
-    })
-    .catch(function(error) {
-      next(error);
-    });
-  } else {
-    next();
-  }
+  console.log("REMOVE HOOK THING")
+  mongoose.model('User').findById(warble.user_id).then(function(user){
+    user.warbles.pull(warble._id);
+    return user.save();
+  })
+  .then(function() { next() });
 });
 
 var Warble = mongoose.model('Warble', warbleSchema);
